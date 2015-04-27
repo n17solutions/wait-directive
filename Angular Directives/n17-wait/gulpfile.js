3b0f5fc12228c0e6250c0e961c494c0b3cb43f87
James Woodley <jwoodley@salesforce.com>
2015-04-27 09:51:41 +0100

    Compressed Stylus

--- Angular Directives/n17-wait/gulpfile.js
+++ Angular Directives/n17-wait/gulpfile.js
@@ -1,9 +1,17 @@
-//var stylus = require('gulp-stylus');
 var gulp 	= require('gulp'),
-	stylus 	= require('gulp-stylus');
+	stylus 	= require('gulp-stylus')
+	uglify	= require('gulp-uglify');
 
 gulp.task('stylus', function() {
 	gulp.src('./stylus/**/*.styl')
 		.pipe(stylus())
 		.pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('stylus-compress', function() {
+	gulp.src('./stylus/**/*.styl')
+		.pipe(stylus({ compress: true }))
+		.pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('production', ['stylus-compress']);
